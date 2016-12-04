d5be6803b2e7facb264b7d59ea3bb846c42fa283
Lavar Askew <open.hyperion@gmail.com>
2016-12-04 17:14:21 -0500

    Initial commit for PureStorage SDK driver code.

---
+++ exportLibraries/storagedrivers/purestorage/src/main/java/open/hyperion/purestorage/impl/PureStorageStorageDriver.java
@@ -0,0 +1,43 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Lavar Askew (open.hyperion@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package open.hyperion.purestorage.impl;
+
+import com.emc.storageos.storagedriver.AbstractStorageDriver;
+import com.emc.storageos.storagedriver.BlockStorageDriver;
+import com.emc.storageos.storagedriver.DefaultStorageDriver;
+import com.emc.storageos.storagedriver.DriverTask;
+
+/**
+ * 
+ * Implements functions to discover the HP 3PAR storage and provide provisioning
+ * You can refer super class for method details
+ *
+ */
+public class PureStorageStorageDriver extends DefaultStorageDriver implements BlockStorageDriver {
+
+    private static final String PURESTORAGE_CONF_FILE = "purestorage-conf.xml";
+	private static final Logger _log = LoggerFactory.getLogger(PureStorageStorageDriver.class);
+	private ApplicationContext parentApplicationContext;
+}
